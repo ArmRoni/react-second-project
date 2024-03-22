4f2508c214975047330346671666028db4ca4862
ArmRoni <ronimia5141@gmail.com>
2024-03-23 00:01:05 +0600

    react project update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
+import Friends from './Friends'
+
+function App() {
+  return (
+    <>
+      <Users></Users>
+      <Counter></Counter>
+      <Team></Team>
+      <Friends></Friends>
+  
+    </>
+  )
+}
+
+export default App
